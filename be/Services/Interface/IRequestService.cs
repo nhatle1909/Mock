35867c0b1ea77efdc80c810e1f61fe99e918a424
nle75234 <nle75234@gmail.com>
2024-03-01 22:37:29 +0700

    Deploy

---
+++ be/Services/Interface/IRequestService.cs
@@ -0,0 +1,14 @@
+﻿using Repositories.ModelView;
+
+namespace Services.Interface
+{
+    public interface IRequestService
+    {
+        public Task<string> Payment(string requestID, int TotalPrice);
+        public Task<object> GetAllRequest(int pageIndex, int pageSize, bool isAsc, string sortField, string searchValue, string searchField);
+        public Task<string> AddPendingRequest(string _id,int totalPrice);
+        public Task<string> UpdateStatusRequest(string _id);
+        public Task<string> DeleteRequest(string _id);
+        public Task<string> CheckPayment(string url);
+    }
+}
